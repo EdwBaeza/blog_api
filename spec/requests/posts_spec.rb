be482243c6ea871ced5e8ca6e64bf0df1f9457cf
edwinbaeza <edwinbaeza05@gmail.com>
2020-12-12 04:09:50 -0600

    Fix rspec test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Api::PostsController, type: :request do 
-  describe "GET /Posts" do
-    before { get '/posts' }
+  describe "GET /api/posts" do
+    before { get '/api/posts' }
 
     it "should return OK" do
       payload = JSON.parse(response.body)
-      expect(payload).not_to be_empty
+      expect(payload).to be_empty
       expect(response).to have_http_status(200)
     end
   end
 
   describe "with data in the DB" do
-    let(:posts) { create_list(:post, 10, published: true) }
-    before { get '/posts' }
+    let!(:posts) { create_list(:post, 10, published: true) }
     it "should return all the published posts" do
+      get '/api/posts'
       payload = JSON.parse(response.body)
-      expect(payload).to eq(posts.size)
+      expect(payload.size).to eq(posts.size)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /posts/{id}" do
-    let(:posts) { create(:post) }
+  describe "GET /api/posts/{id}" do
+    let!(:post) { create(:post) }
     it "should return a post" do
-      get "post/#{post.id}"
+      get "/api/posts/#{post.id}"
       payload = JSON.parse(response.body)
-      expect(payload).to eq(posts.size)
+      expect(payload['id']).to eq(post.id)
       expect(response).to have_http_status(200)
     end
   end
